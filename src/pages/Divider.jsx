e583fb07f030a1dafa2295b25f331f079ec5b03e
jigooo <h00kk@naver.com>
2022-10-18 17:40:51 +0900

    1018

---
+++ src/pages/Divider.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Divider = () => {
+  return (
+    <div className="Divider">
+      <img
+        src={process.env.PUBLIC_URL + "/assets/image/divider.png"}
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Divider;
